a29018b5642230f03971ac9a975ee3f6da175532
Justin Risch <JustinRisch@ipponusa.com>
2015-01-22 23:04:49 -0500

    Bug Fix and ball change
    made the ball a radio button (again). The java logo was just… cheesy.
    
    Also, fixed a collision bug that would make the ball freeze for one
    cycle when it collided with anything. Much more fluid now.

--- src/comps/Ball.java
+++ src/comps/Ball.java
@@ -2,21 +2,17 @@ package comps;
 
 import java.awt.Desktop;
 import java.awt.Dimension;
-
 import java.awt.Insets;
 import java.awt.Rectangle;
 import java.net.URI;
 
 import JBreak.JBreak;
-
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
+import javax.swing.JRadioButton;
 
 @SuppressWarnings("serial")
-public class Ball extends JLabel implements Runnable {
+public class Ball extends JRadioButton implements Runnable {
 
-	// making a singleton to avoid waste! :D 
-	private int x=50, y=150, width = 50, height = 60;
+	private int x=50, y=150, width = 23, height = 20;
 	private double speed = 4;
 	private double xspeed=1, yspeed=1; 
 	private boolean up=false, right=true; 
@@ -25,7 +21,7 @@ public class Ball extends JLabel implements Runnable {
 		if (JBreak.gameOver)
 			return;
 		Rectangle ballrect = this.getBounds();
-	
+
 		if (ballrect.intersects(r))
 		{  
 			up = !up; 
@@ -46,7 +42,29 @@ public class Ball extends JLabel implements Runnable {
 			tempy=y-(int)(yspeed*speed);
 		else
 			tempy=y+(int)(yspeed*speed);
-
+		
+		if (tempx<0 || tempx>427){
+			right=!right;
+			if (right)
+				tempx=x+(int)(xspeed*speed);
+			else 
+				tempx=x-(int)(xspeed*speed);
+		}
+		if (tempy<0) {
+			up=!up;
+			if (up)
+				tempy=y-(int)(yspeed*speed);
+			else
+				tempy=y+(int)(yspeed*speed);
+			
+		} else if (tempy>250){
+			speed =0;
+			JBreak.gameOver=true;
+			JBreak.endScreen.setText("Game Over! Your score was "+JBreak.score+"!");
+			JBreak.endScreen.revalidate();
+			JBreak.endScreen.repaint();
+			return;
+		}
 
 		for (Brick[] b : JBreak.brick)
 			for (Brick brick : b)
@@ -69,30 +87,16 @@ public class Ball extends JLabel implements Runnable {
 						if (start) {
 							Desktop desk = Desktop.getDesktop();
 							try { 
-							URI uri = new URI("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-							Thread.sleep(3000);
-							desk.browse(uri);
+								URI uri = new URI("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+								Thread.sleep(3000);
+								desk.browse(uri);
 							}catch(Exception e){}
 						}
 						return;
 					}
 				}
 			}
-		if (tempx<0 || tempx>427){
-			right=!right;
-			return;
-		}
-		if (tempy<0) {
-			up=!up;
-			return;
-		} else if (tempy>250){
-			speed =0;
-			JBreak.gameOver=true;
-			JBreak.endScreen.setText("Game Over! Your score was "+JBreak.score+"!");
-			JBreak.endScreen.revalidate();
-			JBreak.endScreen.repaint();
-			return;
-		}
+		
 		x=tempx;
 		y=tempy;
 		this.setBounds(x, y, width, height);
@@ -104,15 +108,13 @@ public class Ball extends JLabel implements Runnable {
 	public Ball(Insets insets, Dimension size){
 		this.setVisible(true);
 		this.setEnabled(false);
-		ImageIcon icon =new ImageIcon("images/ball.gif");
-		this.setIcon(icon);
-		//this.setSelected(true);
+		this.setSelected(true);
 		this.revalidate();
 		this.repaint();
 		this.setBounds(x+insets.left, y+insets.top, width, height);
 
 	}
-	
+
 	@Override
 	public void run() {
 		while (true){
